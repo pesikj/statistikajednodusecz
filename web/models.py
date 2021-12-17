730c3c79986fd1313c8e6cbbcbecb4c7f4649618
Jiří Pešík <jiri.pesik@outlook.cz>
2021-12-18 00:29:26 +0100

    Migrated to Bootstrap (#1)
    * Migrated to Bootstrap
    
    * Template update
    
    * Template update
    
    Co-authored-by: Jiří Pešík <jiri.pesik@gmail.com>

--- web/models.py
+++ web/models.py
@@ -1,7 +1,14 @@
 from django.db import models
-
+from markdown2 import Markdown
 
 class Article(models.Model):
     slug = models.CharField(max_length=200)
     content = models.TextField()
     title = models.CharField(max_length=200)
+
+    @property
+    def content_html(self):
+        markdowner = Markdown()
+        content = markdowner.convert(self.content)
+        content = content.replace("<p>", '<p class="fs-5 mb-4">')
+        return content
--- web/views.py
+++ web/views.py
@@ -11,14 +11,7 @@ class ArticleView(DetailView):
     template_name = "article.html"
     model = models.Article
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        t = Template(self.object.content)
-        c = Context({})
-        context["article_content"] = t.render(c)
-        return context
-
     def get_object(self):
-        obj = models.Article.objects.filter(slug=self.kwargs["slug"]).first()
+        obj = models.Article.objects.first()
         return obj
 
