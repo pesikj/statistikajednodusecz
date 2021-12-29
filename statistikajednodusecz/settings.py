dea465d662759126a3ad7467c547c55c9ccc0499
Jiří Pešík <jiri.pesik@gmail.com>
2021-12-29 14:04:34 +0100

    feat:Docker deployment

--- statistikajednodusecz/settings.py
+++ statistikajednodusecz/settings.py
@@ -131,7 +131,11 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = "media/"
 MEDIA_URL = "/media/"
