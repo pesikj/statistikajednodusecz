7ae0fb4ebd95bff3b597d001edaa7b2d7c50a979
Jiří Pešík <jiri.pesik@outlook.cz>
2022-01-16 18:59:44 +0100

    Default URL redirect

--- web/urls.py
+++ web/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,4 +11,6 @@ urlpatterns = [
     path('article/<slug:slug>', views.ArticleView.as_view(), name='article'),
     path('section/<slug:slug>', views.SectionView.as_view(), name='section'),
     path('all-articles/', views.AllArticlesView.as_view(), name='all_articles'),
+    # Temporary link because of links from old version
+    url(r'^(.*)$', views.IndexView.as_view(), name='default'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
