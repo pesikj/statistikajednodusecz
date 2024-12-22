fc99cc363bad271a3a110ac53bb3f1de899319b2
Jiří Pešík <jiri.pesik@outlook.cz>
2024-12-22 21:07:03 +0100

    Fixed debug

--- statistikajednodusecz/settings.py
+++ statistikajednodusecz/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", True)
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["statistikajednoduse.cz", "www.statistikajednoduse.cz", "localhost", "127.0.0.1"]
 
