0c67c4e39bdc875b5156f67df870007ef4682e79
Jiří Pešík <jiri.pesik@outlook.cz>
2024-12-27 14:26:32 +0100

    Added Adsense

--- web/urls.py
+++ web/urls.py
@@ -1,12 +1,14 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path("ads.txt", RedirectView.as_view(url=staticfiles_storage.url("ads.txt")),),
     path('article/<slug:slug>', views.ArticleView.as_view(), name='article'),
     path('section/<slug:slug>', views.SectionView.as_view(), name='section'),
     path('all-articles/', views.AllArticlesView.as_view(), name='all_articles'),
